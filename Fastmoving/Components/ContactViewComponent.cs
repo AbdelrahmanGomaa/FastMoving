2f0e3a14bca1d06f77297d1940b5e279fe621ef7
Abdlrahman Magdy Gomaa <gomaa38@gmail.com>
2025-10-06 19:17:47 +0300

    10/6/2025

---
+++ Fastmoving/Components/ContactViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Fastmoving.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models;
+
+namespace Fastmoving.Components
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("~/Views/Partials/Forms/ContactForm.cshtml", new ContactFormViewModel());
+        }
+    }
+}
---
+++ Fastmoving/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using Umbraco.Cms.Infrastructure.Persistence;
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+System.Data.Common.DbProviderFactories.RegisterFactory(
+    "Microsoft.Data.SqlClient",
+    Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddComposers()
+    .Build();
+
+// تهيئة MVC
+builder.Services.AddMvc(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(5); // أو أي مدة تحبها
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+//builder.WebHost.UseUrls("http://*:5001");
+
+WebApplication app = builder.Build();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+// تأكد من استخدام الـ session بعد ما يتم بناء التطبيق بالكامل
+app.UseSession();
+
+await app.BootUmbracoAsync();
+
+//الآن يمكنك استخدام IUmbracoDatabaseFactory و باقي الخدمات بشكل آمن
+using (var scope = app.Services.CreateScope())
+{
+    var dbFactory = scope.ServiceProvider.GetRequiredService<IUmbracoDatabaseFactory>();
+
+    // تقدر تنفذ أي أوامر على dbFactory هنا إذا كنت محتاج
+}
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
